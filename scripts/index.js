69065584caaf266631b8f63aae7f1d95c2afe5ed
Fabiana Storer <fabiana.storer@gmail.com>
2024-07-27 17:41:44 +0200

    hotfix

--- scripts/index.js
+++ scripts/index.js
@@ -1,93 +1,36 @@
-import Card from "./card.js";
 
-import FormValidator from "./FormValidator.js";
+const editButton = document.querySelector("#edit-button");
+const saveButton = document.querySelector(".popup__save-button");
+const closeButton = document.querySelector("#close-button");
+const formElement = document.querySelector(".popup__form");
 
-import { closePopup } from "./utils.js";
+function openPopUp() {
+    const popup = document.querySelector(".popup");
+    popup.classList.add("popup__opened");
+}
 
-const cardZone = document.querySelector(".elements");
-const buttonAddCard = document.querySelector(".profile__add-button");
-const cardPopup = document.querySelector("#popup-card");
-const formCardPopup = document.querySelector(".popup__card-form");
-const inputCardTitle = document.querySelector(".popup__card-title");
-const inputUrl = document.querySelector(".popup__card-url");
-const buttonCloseAddCard = document.querySelector(".popup__card-button-closed");
+function closePopUp() {
+    const popup = document.querySelector(".popup");
+    popup.classList.remove("popup__opened");
 
-const initialCards = [
-  {
-    name: "Vale de Yosemite",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
-  },
-  {
-    name: "Lago Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
-  },
-  {
-    name: "Montanhas Carecas",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
-  },
-  {
-    name: "Parque Nacional da Vanoise ",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
-  },
-];
+    document.querySelector("#name").value = "";
+    document.querySelector("#about").value = "";
+}
 
-initialCards.forEach((item) => {
-  const card = new Card(item, ".template-card"); //const template = document.querySelector(".template-card");
-  const cardElement = card.generateCard();
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault();
 
-  cardZone.append(cardElement);
-});
+    const nameInput = document.querySelector("#name").value;
+    const aboutInput = document.querySelector("#about").value;
+    const userName = document.querySelector(".profile-info__name");
+    const userAbout = document.querySelector(".profile-info__explore");
 
-formCardPopup.addEventListener("submit", function (evt) {
-  evt.preventDefault();
+    userName.textContent = nameInput;
+    userAbout.textContent = aboutInput;
 
-  const titleInput = document.querySelector("#title");
-  const linkInput = document.querySelector("#image-link");
+    closePopUp();
+}
 
-  const titleValue = inputCardTitle.value;
-  const linkValue = inputUrl.value;
-
-  const newCardData = {
-    name: titleValue,
-    link: linkValue,
-  };
-
-  const card = new Card(newCardData, ".template-card"); //const template = document.querySelector(".template-card");
-  const cardElement = card.generateCard();
-
-  cardZone.prepend(cardElement);
-
-  closePopup(cardPopup);
-});
-
-const validationSettings = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error-visible",
-};
-
-const forms = Array.from(
-  document.querySelectorAll(validationSettings.formSelector)
-);
-forms.forEach((formElement) => {
-  const formValidator = new FormValidator(validationSettings, formElement);
-  formValidator.enableValidation();
-});
-
-const popupOverlay = document.querySelectorAll(".popup__overlay");
-popupOverlay.forEach((overlay) => {
-  overlay.addEventListener("click", () => {
-    closePopup(overlay.parentNode);
-  });
-});
+editButton.addEventListener("click", openPopUp);
+closeButton.addEventListener("click", closePopUp);
+formElement.addEventListener("submit", handleProfileFormSubmit);
